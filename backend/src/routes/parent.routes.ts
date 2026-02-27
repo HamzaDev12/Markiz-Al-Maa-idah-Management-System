e7c038426ece017ad2a385ca388535339b809995
Hamza Mustafa <hmustafe299@gmail.com>
2026-02-27 12:09:37 +0300

    pendding parent controller

--- backend/src/routes/parent.routes.ts
+++ backend/src/routes/parent.routes.ts
@@ -2,7 +2,13 @@ import { Router } from "express";
 import { authenticationMiddleware } from "../middlewares/auth.middleware.js";
 import { authorized } from "../middlewares/authorized.middleware.js";
 import { Role } from "../generated/prisma/enums.js";
-import { createParent } from "../controllers/parent.controller.js";
+import {
+  createParent,
+  deleteParent,
+  getAllParenst,
+  getParentById,
+  updateParent,
+} from "../controllers/parent.controller.js";
 const route = Router();
 
 route.post(
@@ -10,6 +16,34 @@ route.post(
   authenticationMiddleware,
   authorized([Role.ADMIN]),
   createParent,
+);
+
+route.patch(
+  "/update/:id",
+  authenticationMiddleware,
+  authorized([Role.ADMIN]),
+  updateParent,
+);
+
+route.delete(
+  "/delete/:id",
+  authenticationMiddleware,
+  authorized([Role.ADMIN]),
+  deleteParent,
+);
+
+route.get(
+  "/getAll",
+  authenticationMiddleware,
+  authorized([Role.ADMIN]),
+  getAllParenst,
+);
+
+route.get(
+  "/getParentById/:id",
+  authenticationMiddleware,
+  authorized([Role.PARENT]),
+  getParentById,
 );
 
 export default route;
